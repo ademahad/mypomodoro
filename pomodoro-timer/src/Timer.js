271cee2a7ce9a3c02aa2dd4ff27c227f329b833d
Mahad Ade <mahadade321@gmail.com>
2022-08-12 23:12:09 -0500

    Added timer

---
+++ pomodoro-timer/src/Timer.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const red = '#f53e4e';
+const green = '#4aec8c';
+
+
+
+function Timer() {
+    return (
+        <div>
+            <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+            textColor: '#fff',
+            pathColor: red,
+            tailColor: 'rgba(255,255,255,.2)',
+        })} />
+        </div>
+    );
+}
+
+export default Timer;
