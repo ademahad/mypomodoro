f25898841e9834131149f329847a2a0f41667560
Mahad Ade <mahadade321@gmail.com>
2022-08-12 23:44:48 -0500

    Added play button

--- pomodoro-timer/src/Timer.js
+++ pomodoro-timer/src/Timer.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
 
 const red = '#f53e4e';
-const green = '#4aec8c';
+// const green = '#4aec8c';
 
 
 
@@ -14,7 +15,11 @@ function Timer() {
             textColor: '#fff',
             pathColor: red,
             tailColor: 'rgba(255,255,255,.2)',
-        })} />
+            })} />
+            <div style={{marginTop:'20px'}}>
+                <PlayButton />
+            </div>
+
         </div>
     );
 }
